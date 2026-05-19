01a96cfcc7ceee6697aa271c363aeeffa3efdbe1
Storiesbyabi <sabhijith595@gmail.com>
2026-05-19 17:29:27 +0530

    sales order to invoice pending

--- invoice_for_multiple_sales_order/models/account_move.py
+++ invoice_for_multiple_sales_order/models/account_move.py
@@ -5,13 +5,42 @@ from odoo import fields, models, api
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    related_so_to_invoice_ids = fields.Many2many(comodel_name='sale.order',string='So',domain="[('invoice_status','=','to invoice')]")
+    related_so_to_invoice_ids = fields.Many2many(comodel_name='sale.order',string='So',domain="[('invoice_status','=','to invoice'),('partner_id','=',partner_id)]")
 
     @api.onchange('related_so_to_invoice_ids')
     def invoice_lines_data(self):
-        so_id = self.related_so_to_invoice_ids.ids
+        so_id = self.related_so_to_invoice_ids
 
-        sale_order_line = self.env['sale.order.line'].search([('order_id','in',so_id)])
+        print('sale order',so_id)
 
-        self.invoice_line_ids = [fields.Command.create({
-                'product_id': sale_order_line.product_template_id})]
+        sale_order_id = so_id.order_line
+        print(sale_order_id)
+
+        if sale_order_id:
+            for i in sale_order_id.product_id.ids:
+                self.invoice_line_ids = [fields.Command.create({
+                    'product_id': i
+
+                })]
+                for i in self.invoice_line_ids.ids:
+                    print(i)
+                    so_id.order_line.invoice_lines = [fields.Command.link(
+                        i
+                    )]
+            self.move_type = 'out_invoice'
+
+            print('move_type',self.move_type)
+
+
+
+            print(22,self.invoice_line_ids.ids)
+
+            print('invoice_ids',so_id.order_line.invoice_lines)
+
+
+
+
+
+
+
+
