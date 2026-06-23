b5b50443b3f129fd4ff68c135f749ac6c6496ef9
Storiesbyabi <sabhijith595@gmail.com>
2026-06-23 17:33:00 +0530

    Validate approval btn based on flags

--- partial_delivery_control/models/stock_picking.py
+++ partial_delivery_control/models/stock_picking.py
@@ -1,3 +1,5 @@
+
+
 from odoo import api, fields, models
 
 class StockPicking(models.Model):
@@ -7,23 +9,27 @@ class StockPicking(models.Model):
         ('not_approval', 'Waiting for approval'),('done',)
     ])
 
-    approve_flag = fields.Boolean()
+    approve_flag = fields.Boolean(default=True)
 
 
 
-    def button_validate(self):
+    def validate_button(self):
         for rec in self.move_ids:
-            if not rec.product_id.allow_partial_delivery:
+            if not rec.product_id.allow_partial_delivery and approve_flag:
+                print('in', self.approve_flag)
                 if rec.product_uom_qty != rec.quantity:
                     print('pd', rec.product_uom_qty)
                     print('pd', rec.quantity)
-                return self._action_generate_backorder_wizard()
-            else:
-                return super().button_validate()
+                return super(StockPicking, self.with_context(hide_button=False)).validate_button()
 
+            else:
+                self.approve_flag = True
+                print('out', self.approve_flag)
+                return super(StockPicking, self.with_context(hide_button=True)).validate_button()
 
     def action_approve_req(self):
-        return super().button_validate()
+        self.approve_flag = False
+        return self.validate_button()
 
 
 
--- partial_delivery_control/wizard/partial_delivery.py
+++ partial_delivery_control/wizard/partial_delivery.py
@@ -7,7 +7,21 @@ class PartialDeliveryWizard(models.TransientModel):
 
     _inherit = 'stock.backorder.confirmation'
 
+    approve_flag = fields.Boolean(default=lambda self: self.env.context.get('hide_button'))
+
+    # def _compute_flag(self):
+    #     for rec in self:
+    #         rec.pick_ids.approve_flag
+    #         print('flag',rec.pick_ids.approve_flag)
+
+
+
+
     def action_request(self):
-        print('id',self.pick_ids)
+        print('id',self.backorder_confirmation_line_ids.picking_id.product_id)
+        print('context',self.pick_ids.approve_flag)
+        print('context1',self.env.context.get('hide_button'))
+        print('context2', self.approve_flag)
         self.pick_ids.state = 'not_approval'
         pass
+
